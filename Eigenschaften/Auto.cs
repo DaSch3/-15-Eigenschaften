51759038d5456593829e5b8701333222ee798e89
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:35:19 +0200

    Projektdateien hinzufügen.

---
+++ Eigenschaften/Auto.cs
@@ -0,0 +1,14 @@
+﻿namespace Eigenschaften
+{
+    class Auto
+    {
+        //Variablen
+        private bool motorAn;
+        private bool scheibenwischerAn;
+
+        //Eigenschaften
+        public string Farbe { get; set; }
+        public int PS { get; set; }
+        public int AnzahlTüren { get; set; }
+    }
+}
---
+++ Eigenschaften/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eigenschaften
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car = new Auto();
+
+            car.Farbe = "Grün";
+            Console.WriteLine(car.Farbe);
+
+            Console.ReadKey();
+        }
+    }
+    
+    class Auto
+    {
+        //Variablen
+        private bool motorAn;
+        private bool scheibenwischerAn;
+
+        //Eigenschaften
+        public string Farbe { get; set; }
+        public int PS { get; set; }
+        public int AnzahlTüren { get; set; }
+    }
+}
